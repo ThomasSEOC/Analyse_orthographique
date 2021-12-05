5ac7c413673ea9420188155e98226f3f3a57dbb7
ThomasD <thomas.dijs@phelma.grenoble-inp.fr>
2021-12-05 17:57:22 +0100

    Début hashtable

--- src/hashtable.c
+++ src/hashtable.c
@@ -1,19 +1,115 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "hashtable.h"
 #include <string.h>
+#include "hashtable.h"
 
-int hash (char* ch, int size){
-  //hache le mot:
-  int hash = 0;
-  int n = strlen (ch);
-  int i = 0;
-  int c;
-  for (i = 0; i < n; i++){
-    c = ch[i];
-    hash += c * (i+1);
+#define PREMIER 13
+#define MODULO 200
+
+int puissance(int x, int y){
+  int resultat=1;
+    for(int i = 0; i<y;i++){
+      resultat *=x;
+    }
+    return resultat;
+}
+
+void insere_tete(T nouveau, liste* pl){
+  liste p = malloc(sizeof(*p));
+
+  if (*pl == NULL){
+    p -> val = nouveau;
+    *pl = p;
+    return;
   }
-  hash = hash % size;
+
+  if(p == NULL){
+    fprintf( stderr, "Fatal: Unable to allocate new list link.\n" );
+    exit(EXIT_FAILURE);
+  }
+
+  p->val = nouveau ;
+  p->next = *pl ;
+  *pl = p ;
+}
+
+
+void affiche_liste (liste l){
+  liste p = l ;
+  while(p -> next != NULL){
+    printf("mot : %s | ", p->val);
+    p = p -> next;
+  }
+  printf("\n");
+}
+
+int hash (T word){
+  int hash = 0;
+  for (int i = 0; i < strlen(word); i++){
+    hash += word[i]*puissance(PREMIER,i);
+  }
+  hash %= MODULO;
   return hash;
+}
+
+bool identiques(T elem_1, T elem_2){
+  if(strcmp(elem_1,elem_2)==0){
+    return true;
+  }
+  return false;
+}
+
+bool est_present(T elem, table_hachage* ht){
+  int hash_value = hash(elem);
+  liste l=ht->table[hash_value];
+  while (l != NULL){
+    if(identiques(elem,l->val)){
+      return true;
+    }
+    l=l->next;
+  }
+  return false;
+}
+
+void inserer_sans_redimensionner(T element, table_hachage* ht){
+  int hash_value = hash(element);
+  liste l=ht->table[hash_value];
+  ht->nb_elements++;
+  insere_tete(element,&l);
+}
+
+void afficher_table(table_hachage* ht){
+  printf("{\n");
+  for(int i=0 ; i<ht->capacite ; i++)
+  {
+    if(ht->table[i]!=NULL)
+    {
+      printf("{ hash n°%d :",i);
+      affiche_liste(ht->table[i]);
+      printf("}\n");
+    }
+  }
+  printf("}\n");
+}
+
+
+
+
+int main(){
+  liste l;
+  //l->val = "loic";
+  insere_tete("hello",&l);
+  insere_tete("mme",&l);
+  insere_tete("thomas",&l);
+  insere_tete("ich",&l);
+  insere_tete("ekip",&l);
+
+  affiche_liste(l);
+
+
+  printf("%d hash de %s\n",hash("salut"),"salut");
+  printf("%d hash de %s\n",hash("bonjour"),"bonjour");
+
+  return EXIT_SUCCESS;
 }
--- src/hashtable.h
+++ src/hashtable.h
@@ -2,6 +2,24 @@
 #ifndef __CONSOLE_H__
 #define __CONSOLE_H__
 
-int hash (char* ch, int size);
+
+
+typedef char* T;
+
+typedef struct _cellule {
+  T val;
+  struct _cellule* next;
+} cellule;
+typedef cellule* liste;
+
+typedef struct {
+  liste* table;
+  unsigned capacite; /* capacité de la table */
+  unsigned nb_elements; /* nombres d'éléments dans la table */
+  unsigned capacite_initiale; /* utile lors du redimensionnement */
+} table_hachage;
+
+
+int hash (char* ch);
 
 #endif
